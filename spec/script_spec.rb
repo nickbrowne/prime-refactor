9293cba9bc88c968bb8de8e7e84d637886715391
nickbrowne <nick.browne@theconversation.edu.au>
2016-04-06 12:43:41 +1000

    Add some more tests to cover "error" cases

--- spec/script_spec.rb
+++ spec/script_spec.rb
@@ -18,4 +18,16 @@ RSpec.describe "#first_n_primes" do
       expect(first_n_primes(10)).to eq [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
     end
   end
+
+  context "n is not an integer" do
+    it "returns a string with an error message" do
+      expect(first_n_primes("bean sprout")).to eq "n must be an integer."
+    end
+  end
+
+  context "n less than 1" do
+    it "returns a string with an error message" do
+      expect(first_n_primes(0)).to eq "n must be greater than 0."
+    end
+  end
 end
